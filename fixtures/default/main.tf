421bb0e9e923287123c0e417077a349dc1184e92
Brett Curtis <brett@osinfra.io>
2024-04-14 08:04:24 -0400

    Deprecation of Kitchen-Terraform (#38)

--- fixtures/default/main.tf
+++ fixtures/default/main.tf
@@ -3,7 +3,7 @@ module "storage_bucket" {
   # This module will be consumed using the source address of the github repo and not the "../../../" used in this test.
   # source = "github.com/osinfra-io/terraform-google-storage-bucket?ref=v0.0.0"
 
-  source = "../../../"
+  source = "../../"
 
   labels = {
     cost-center = "x000"
